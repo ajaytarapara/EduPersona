c85c07e1805b7dcde1b3e40b9052975d3c6d0655
ajaytarapara <ajaytarapara77@gmail.com>
2026-02-25 15:59:34 +0530

    setup redux and persist

---
+++ edu-persona-client/edu-persona-core-client/src/store/index.ts
@@ -0,0 +1,38 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { authReducer } from "./features";
+
+let storeRef: AppStore | null = null;
+
+export const injectStore = (store: AppStore) => {
+  storeRef = store;
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+injectStore(store);
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
